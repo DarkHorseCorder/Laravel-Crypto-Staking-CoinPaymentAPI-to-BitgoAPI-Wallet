825785f8eaee98d92f9bc44e8185ceea1304f9a6
DreamWillBeReal <120799088+DarkHorseCorder@users.noreply.github.com>
2021-05-10 18:29:46 +0900

    model

---
+++ project/app/Models/SiteContent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SiteContent extends Model
+{
+    use HasFactory;
+
+    protected $casts = ['content'=>'object','sub_content'=>'object'];
+}
