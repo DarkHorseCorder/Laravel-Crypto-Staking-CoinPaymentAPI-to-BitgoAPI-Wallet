dd4dca3e97cb96da399b714a33ac0c4af863674d
DreamWillBeReal <120799088+DarkHorseCorder@users.noreply.github.com>
2021-05-21 18:34:16 +0900

    database migration

---
+++ project/database/migrations/2021_12_21_095225_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('trnx');
+            $table->integer('user_id');
+            $table->tinyInteger('user_type');
+            $table->integer('currency_id');
+            $table->integer('wallet_id');
+            $table->decimal('amount',20,10);
+            $table->string('remark');
+            $table->string('details');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
